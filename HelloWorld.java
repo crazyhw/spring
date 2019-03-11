225fc9a049b4a3ad47b7fbeabcabe9a3926f4cea
于靖鑫 <37971412+unlive@users.noreply.github.com>
2019-03-11 20:34:57 +0800

    今日成果
    一个简单程序

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,12 @@
-
+package yjx;
 public class HelloWorld {
+    public String name;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello World!");
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void hello() {
+        System.out.print("hello:" + name);
+    }
 }
