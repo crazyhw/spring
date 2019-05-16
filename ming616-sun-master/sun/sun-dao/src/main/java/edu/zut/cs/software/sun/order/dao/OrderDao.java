e89b72dcd78bc708fb90ae7ce0c4fa0a813578bc
201708044112 <572050969@qq.com>
2019-05-16 21:23:35 +0800

    。

---
+++ ming616-sun-master/sun/sun-dao/src/main/java/edu/zut/cs/software/sun/order/dao/OrderDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.software.sun.order.dao;
+
+import edu.zut.cs.software.base.dao.GenericDao;
+import edu.zut.cs.software.sun.order.domain.Order;
+
+public interface OrderDao extends GenericDao<Order, Long> {
+
+}
