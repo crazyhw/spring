87641a866080c3f0179a1d30de7b9308300a0813
renxinyi <1328018473@qq.com>
2019-06-12 09:59:22 +0800

    删除

--- sea.horse-dao/src/main/java/pioneer/seahorse/pay/dao/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package pioneer.seahorse.pay.dao;
-
-import org.springframework.stereotype.Repository;
-import pioneer.seahorse.base.dao.GenericTreeDao;
-import pioneer.seahorse.pay.domain.Customer;
-
-@Repository("customerDao")
-public interface CustomerDao extends GenericTreeDao<Customer, Long> {
-}
